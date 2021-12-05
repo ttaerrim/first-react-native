fc9d20c601c9f931eedf0f07ca2044a5ff6fb4a1
ttaerrim <ltr0121@naver.com>
2021-12-05 22:52:40 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>아래 버튼을 눌러주세요</Text>
+        {/* 버튼 onPress 속성에 일반 함수를 연결 할 수 있습니다. */}
+        <Button
+          style={styles.buttonStyle}
+          title="버튼입니다 "
+          color="#f194ff"
+          onPress={function () {
+            Alert.alert("팝업 알람입니다!!");
+          }}
+        />
+        {/* ES6 문법으로 배웠던 화살표 함수로 연결 할 수도 있습니다. */}
+        <Button
+          style={styles.buttonStyle}
+          title="버튼입니다 "
+          color="#F19400"
+          onPress={() => {
+            Alert.alert("팝업 알람입니다!!");
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  textContainer: {
+    height: 100,
+    margin: 10,
+  },
+  textStyle: {
+    textAlign: "center",
   },
 });

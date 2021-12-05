c73d7b9da7f0661a968387a8ba4bf395efbb0322
ttaerrim <ttrrr121@gmail.com>
2021-12-05 22:58:17 +0900

    Button 대신 TouchableOpacity 사용

--- App.js
+++ App.js
@@ -3,37 +3,39 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
+  ScrollView,
+  TouchableOpacity,
   Alert,
-  SafeAreaView,
 } from "react-native";
 
 export default function App() {
-  const innerFunction = () => {
-    Alert.alert("팝업 알람입니다!!");
+  const customAlert = () => {
+    Alert.alert("TouchableOpacity에도 onPress 속성이 있습니다");
   };
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.textStyle}>아래 버튼을 눌러주세요</Text>
-        {/* 버튼 onPress 속성에 일반 함수를 연결 할 수 있습니다. */}
-        <Button
-          style={styles.buttonStyle}
-          title="버튼입니다 "
-          color="#f194ff"
-          onPress={innerFunction}
-        />
-        {/* ES6 문법으로 배웠던 화살표 함수로 연결 할 수도 있습니다. */}
-        <Button
-          style={styles.buttonStyle}
-          title="버튼입니다 "
-          color="#F19400"
-          onPress={() => {
-            innerFunction();
-          }}
-        />
-      </View>
-    </SafeAreaView>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.textContainer} onPress={customAlert}>
+        <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
@@ -44,6 +46,9 @@ const styles = StyleSheet.create({
   },
   textContainer: {
     height: 100,
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 10,
     margin: 10,
   },
   textStyle: {

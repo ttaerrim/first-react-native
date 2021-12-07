50b49424f3ea6c0335876d90ee1e7fbe9d2a3340
ttaerrim <ltr0121@naver.com>
2021-12-07 14:45:59 +0900

    DetailPage.js 코드스니펫

--- pages/DetailPage.js
+++ pages/DetailPage.js
@@ -1,16 +1,15 @@
 import React from "react";
-
-import { StatusBar } from "expo-status-bar";
 import {
-  View,
-  Text,
   StyleSheet,
+  Text,
+  View,
   Image,
-  TouchableOpacity,
   ScrollView,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 
-export default function AboutPage() {
+export default function DetailPage() {
   const tip = {
     idx: 9,
     category: "재테크",
@@ -20,58 +19,61 @@ export default function AboutPage() {
     desc: "요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
     date: "2020.09.09",
   };
+
+  const popup = () => {
+    Alert.alert("팝업!!");
+  };
   return (
+    // ScrollView에서의 flex 숫자는 의미가 없습니다. 정확히 보여지는 화면을 몇등분 하지 않고
+    // 화면에 넣은 컨텐츠를 모두 보여주려 스크롤 기능이 존재하기 때문입니다.
+    // 여기선 내부의 컨텐츠들 영역을 결정짓기 위해서 height 값과 margin,padding 값을 적절히 잘 이용해야 합니다.
     <ScrollView style={styles.container}>
-      <StatusBar style="light" />
-      <Image
-        style={styles.detailImage}
-        source={{ uri: tip.image }}
-        resizeMode={"cover"}
-      />
-      <Text style={styles.title}>{tip.title}</Text>
-      <Text style={styles.desc}>{tip.desc}</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonTitle}>팁 찜하기</Text>
-      </TouchableOpacity>
+      <Image style={styles.image} source={{ uri: tip.image }} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{tip.title}</Text>
+        <Text style={styles.desc}>{tip.desc}</Text>
+        <TouchableOpacity style={styles.button} onPress={() => popup()}>
+          <Text style={styles.buttonText}>팁 찜하기</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "#000",
   },
-  detailImage: {
-    width: "99%",
+  image: {
     height: 400,
-    alignSelf: "center",
+    margin: 10,
     marginTop: 40,
     borderRadius: 20,
-    marginBottom: 40,
+  },
+  textContainer: {
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    color: "white",
     fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: "700",
+    color: "#eee",
   },
   desc: {
-    color: "white",
-    margin: 20,
+    marginTop: 10,
+    color: "#eee",
   },
   button: {
     width: 100,
-    height: 40,
-    alignSelf: "center",
-    borderColor: "pink",
+    marginTop: 20,
+    padding: 10,
     borderWidth: 1,
-    borderRadius: 10,
+    borderColor: "deeppink",
+    borderRadius: 7,
   },
-  buttonTitle: {
-    marginTop: 10,
-    color: "white",
+  buttonText: {
+    color: "#fff",
     textAlign: "center",
-    alignItems: "center",
   },
 });
